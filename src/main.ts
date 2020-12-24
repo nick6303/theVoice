29178354474c7292b59678cc2c439834ea48133d
Nick <20190801@cyadmk.com>
2020-12-24 14:24:59 +0800

    暫存

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+import './assets/css/reset.css'
+
 import { createApp } from 'vue'
+import { createModal } from '@act/slime-modal'
+
+import { InitResolve } from './types'
+
 import App from './App.vue'
 
-import store from './store/index.ts'
+import store from './store'
+
 
 const app = createApp(App)
-app.use(store).mount('#app')
+
+app.use(store).use(createModal('modal')).mount('#app')
