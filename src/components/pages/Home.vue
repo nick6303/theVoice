17f16640a10a1e69816dc70c7dd7eefbca66c01b
nick <nick@example.com>
2021-01-11 22:23:45 +0800

    update

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -35,8 +35,9 @@ export default defineComponent({
 
     const submit = () => {
       if (account.value) {
+        const path = process.env.NODE_ENV === 'production' ? '/mywork/theVoice':''
         // 判斷是否已填過調查或資格符不符合
-        router.push('/survey')
+        router.push(`${path}/survey`)
       }
     }
     return {
